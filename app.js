266e645f30f49b5e4cabe8ae191f574c4ccb932a
Daryl Kranec <dkranec@gmail.com>
2016-01-31 15:31:43 -0500

    lots of updates :-/

--- app.js
+++ app.js
@@ -1,46 +1,52 @@
-var Client = require('ftp');
+'use strict'
+
+var JSFtp = require('jsftp');
 var inspect = require('util').inspect;
 var fs = require('fs');
+var async = require('async');
+var EDI = require('edi');
 
-c = new Client()
+var ftp = new JSFtp(require('./util/ftp'))
+var local = 'EDI/mohawk/OUTBOX/'
+var remote = 'OUTBOX'
 
-c.on('ready', function(){
-  var downloads = [], fileNames = [];
-	c.list('/*', function(err, list) {
-    var cwd = '';
-		console.log(list)
-      if (err) throw err;
-      cwd = 'INBOX/'
-      c.list(cwd+ '*', function(err, list) {
-        console.log(cwd)
-      	if (err) throw err;
-     	 console.log(list);
-    	})
-      cwd = 'OUTBOX/'
-      c.list(cwd + '*', function(err, list) {
-        console.log(inspect(list))
-      	if (err) throw err;
-        for(var i=0;i<list.length-1;i++){
-          console.log(i)
-          console.log(list[i].name)
-          if(list[i].name){
-            c.get(cwd + list[i].name, function(err, stream){
-                if(err){
-                    console.log('Error getting ' + cwd + list[i].name)
-                    throw err;
-                }else{
-                    console.log(i)
-                    console.log('downloading ' + cwd + list[i].name)
-                    stream.pipe(fs.createWriteStream(cwd + list[i].name))
-                }
-            })
-          }
-        }
-        c.end()
-    	})
-        
-  
-    })
-
+var gatherFiles = function(dir){
+  return new Promise(function(resolve, reject){
+    ftp.ls(dir + '/*', function(err, res) {
+    if (err) reject(err)
+    var remoteFiles = [];
+    res.forEach(function(file){
+      remoteFiles.push(file.name)
+    });
+    resolve(remoteFiles)
+  })
 })
-c.connect(require('./util/ftp'))
+}
+
+gatherFiles(remote).then(function(files){
+  console.log(files)
+  async.mapLimit(files, 1, function(file, callback){
+    console.log('attempting: ' +remote + file + '->' + local + file)
+    ftp.get(remote +'/'+ file, local +'/'+ file, function(err){
+      if(err){
+        console.log('Error getting ' + file)
+        callback(err)
+      }else{
+       console.log('Got ' + file)
+       callback()
+      }
+
+    })
+  }, function(err, res){
+    if(err){
+      console.log(err)
+    }
+    console.log('updates complete' + res)
+    
+  })
+})
+
+
+
+
+
