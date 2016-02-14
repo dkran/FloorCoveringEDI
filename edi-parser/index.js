0146b9a326d1de55774bb9f66d4ab41ffddf6439
Daryl Kranec <dkranec@gmail.com>
2016-02-13 22:36:15 -0500

    Setting up parser to find transactional sets.

--- edi-parser/index.js
+++ edi-parser/index.js
@@ -8,11 +8,16 @@ var fs = require('fs'),
    this.file = ''
    this.lines = []
    this.segments = []
-   this.object = {}
+   this.object = {
+     invoices: [],
+     salesCatalog: [],
+     manifest: []
+   }
  }
  EDI.prototype.loadData = function(string){
    this.file = fs.readFileSync(string, 'utf8')
    this.getLines()
+   this.getSets()
  }
  
  EDI.prototype.getLines = function(){
@@ -40,4 +45,14 @@ var fs = require('fs'),
    console.log(this.object)
  }
  
+ 
+ EDI.prototype.getSets = function(){
+   var sets = []
+   for(var i = 0; i < this.lines.length; i++){
+     if(this.lines[i].split('*')[0].trim() === 'ST' || this.lines[i].split('*')[0].trim() === 'SE'){
+       sets.push(i)
+     }
+   }
+   console.log(sets)
+ }
  module.exports = EDI
