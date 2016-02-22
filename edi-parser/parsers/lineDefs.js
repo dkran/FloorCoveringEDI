5397579130d2d2b8157a8730a9e71ede260ec58e
Daryl Kranec <dkranec@gmail.com>
2016-02-22 18:20:04 -0500

    Tinkering with the possibility of an easier generation of different EDI types. I am crazy, but this is the next step.  Watch me as I burn.

---
+++ edi-parser/parsers/lineDefs.js
@@ -0,0 +1,16 @@
+module.exports = {
+  ISA: 'interchangeControlHeader',
+  IEA: 'interchangeControlTrailer',
+  GS: 'groupHeader',
+  GE: 'groupTrailer',
+  ST: 'transactionHeader',
+  SE: 'transactionTrailer',
+  BSN: 'beginningSegment',
+  DTM: 'dateTime',
+  HL: 'heirarchyLevel',
+  LIN: 'itemId',
+  MAN: 'marksAndNumbers',
+  PRF: 'purchaseOrderRef',
+  TD1: 'carrierDetails',
+  TD5: 'carrierRouting'
+}
