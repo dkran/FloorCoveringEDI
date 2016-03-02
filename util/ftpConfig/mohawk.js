9ffc03cdae8543afc740b4308bef6d55b945a28e
Daryl Kranec <dkranec@gmail.com>
2016-03-01 19:19:22 -0700

    Planning to itemize out all providors ftp servers

---
+++ util/ftpConfig/mohawk.js
@@ -0,0 +1,8 @@
+module.exports = {
+	host: 'mohawk1.mohawkind.com',
+	port: 2850,
+	user: 'AFFRDBLFLR',
+	pass: '3EFC7DRZP',
+  local: './EDI/mohawk/OUTBOX',
+  remote: 'OUTBOX'
+}
---
+++ util/ftpConfig/shaw.js
@@ -0,0 +1,8 @@
+module.exports = {
+	host: 'shawedi.shawfloors.com',
+	port: 21,
+	user: 'edi03887',
+	pass: 'aff829',
+  local: './EDI/shaw/OUTBOX',
+  remote: 'OUTBOX'
+}
