ddb2823eaae419cbe839397ffc90f7a6a6d42784
Daryl Kranec <dkranec@gmail.com>
2016-03-11 14:39:05 -0500

    can set terminator / use edi data.

--- process.js
+++ process.js
@@ -3,7 +3,7 @@ var EDI = require('./edi-parser')
 
 var edi = new EDI()
 edi.setTerminator('\n')
-edi.loadData('EDI/shaw/Outbox/000000025.856')
+edi.loadData('EDI/shaw/Outbox/000000037.856')
 
 //console.log('Lines: \r'+edi.lines)
 //console.log('Segments: \r'+edi.segments)
