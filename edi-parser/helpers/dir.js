82f17b37af2037027d0316b8b65189e5b8b14afe
Daryl Kranec <dkranec@gmail.com>
2016-03-27 09:50:43 -0400

    now messing wit' my helpers

--- edi-parser/helpers/dir.js
+++ edi-parser/helpers/dir.js
@@ -6,6 +6,28 @@ var fs = require('fs'),
 
 var Dir = function(){
   this.root = process.env.PWD
+  checkFile('EDI').then(console.log)
+}
+
+
+function checkFile(directory, root){
+  return new Promise(function(resolve, reject){
+    if(!root){
+      fs.stat(this.root+directory, function(err, stats){
+        console.log(stats)
+        if (err) reject(err)
+        if (stats.isDirectory()) resolve(true)
+        else reject(new Error(this.root + directory + 'exists but is not a directory'))
+      })
+    }else{
+      fs.stat(root+directory, function(err, stats){
+        if (err) reject(err)
+        if (stats.isDirectory()) resolve(true)
+        else reject(new Error(root + directory + 'exists but is not a directory'))
+      })
+    }
+  })
+  
 }
 
 module.exports = new Dir()
